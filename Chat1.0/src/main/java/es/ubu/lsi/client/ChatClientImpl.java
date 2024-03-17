f7e174778211887c626510788b08d9fcf4be1091
Daniel <dfb1001@alu.ubu.es>
2024-03-17 20:40:26 +0100

    Definimos los métodos por defecto de la clase ChatClientImpl.java y su clase interna ChatClientListener

--- Chat1.0/src/main/java/es/ubu/lsi/client/ChatClientImpl.java
+++ Chat1.0/src/main/java/es/ubu/lsi/client/ChatClientImpl.java
@@ -1,10 +1,47 @@
 package es.ubu.lsi.client;
 
-public class ChatClientImpl {
+import es.ubu.lsi.common.ChatMessage;
+
+public class ChatClientImpl implements ChatClient {
 	
 	
-	public class ChatClientListener{
+
+	// Método Main
+	public static void main(String[] args) {
 		
 	}
+	
+	@Override
+	public boolean start() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void sendMessage(ChatMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Clase interna ChatClientListener.
+	 * 
+	 */
+	public class ChatClientListener implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
 
 }
