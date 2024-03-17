fbaf45bec4e303834c13bcdea085c1142bb59069
Daniel <dfb1001@alu.ubu.es>
2024-03-17 16:26:32 +0100

    Añadimos los métodos por defecto, la clase interna ServerThreadForClient y algunos atributos de ChatServerImpl.java

--- Chat1.0/src/main/java/es/ubu/lsi/server/ChatServerImpl.java
+++ Chat1.0/src/main/java/es/ubu/lsi/server/ChatServerImpl.java
@@ -1,9 +1,85 @@
 package es.ubu.lsi.server;
 
-public class ChatServerImpl {
+import java.io.*;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
 
+import es.ubu.lsi.common.ChatMessage;
+
+public class ChatServerImpl implements ChatServer{
+
+	// Puerto por defecto
+	final int DEFAULT_PORT = 1500;
 	
-	public class ServerThreadForClient{
+	// Atributos estáticos
+	private static int clientId;
+	private static SimpleDateFormat sdf;
+	
+	// Atributos internos
+	private int port;
+	private boolean alive;
+	
+	// Constructor de la clase
+	public ChatServerImpl(int port) {
+		this.port = port;
+	}
+	
+	// Método Main
+	public static void main(String[] args) {
+		
+	}
+	
+	@Override
+	public void startup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void broadcas(ChatMessage message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	class ChatServerThreadForClient extends Thread{
+		
+		private Socket clientSocket;
+		
+		private int port = ChatServerImpl.this.port;
+				
+		public ChatServerThreadForClient(Socket clientSocket) {
+			this.clientSocket = clientSocket;
+		}
+
+		public void run() {
+			try {
+	            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+	        	BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+				String inputLine;
+	            
+				while ((inputLine = in.readLine()) != null) {
+	            	System.out.println(clientSocket.getPort() + ":" + inputLine);
+	                out.println(inputLine);
+	            }
+	        }
+	        catch (IOException e) {
+	            System.out.println("Exception caught on thread");
+	            System.out.println(e.getMessage());
+	        }
+	      }
 		
 	}
 }
