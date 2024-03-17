af799177031ce9edc20b1c5277843070f05a478a
Daniel <dfb1001@alu.ubu.es>
2024-03-17 16:07:46 +0100

    Incluimos los métodos en la interfaz CharServer

--- Chat1.0/src/main/java/es/ubu/lsi/server/ChatServer.java
+++ Chat1.0/src/main/java/es/ubu/lsi/server/ChatServer.java
@@ -1,5 +1,26 @@
 package es.ubu.lsi.server;
 
+import es.ubu.lsi.common.ChatMessage;
+
+/**
+ * Interfaz ChatServer
+ * 
+ * Contiene la definicion básica de los métodos
+ * 
+ */
+
 public interface ChatServer {
 
+	// Inicio del servidor
+	public void startup();
+	
+	// Apagado del servidor
+	public void shutdown();
+	
+	// Mensaje a todos los clientes
+	public void broadcas(ChatMessage message);
+	
+	// Eliminar un cliente
+	public void remove (int id);
+	
 }
